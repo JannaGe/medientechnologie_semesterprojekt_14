d559e1423aa690916ad5f305b8f427bb7eebfb98
JannaGe <jgeisse@web.de>
2015-01-12 14:56:44 +0100

    aufklappen mit Unterpunkten

---
+++ style/scripts.js
@@ -0,0 +1,21 @@
+<script type="text/javascript">
+
+	function toggle(control){
+		var elem = document.getElementById(control);
+	
+		if(elem.style.display == "none"){
+			elem.style.display = "block";
+		}
+		else{ 
+			elem.style.display = "none";
+		}
+	}
+
+
+
+
+
+
+
+	
+</script>
