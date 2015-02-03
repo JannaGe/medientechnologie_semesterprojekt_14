7257fd77e7fa40b945e842c1d02b2e0f1883e7c0
JannaGe <jgeisse@web.de>
2015-02-03 21:21:27 +0100

    Fertige Webseite – Stand 03.02.2015
    Man kann auf 8 verschiedene Spieler klicken.

--- style/scripts.js
+++ style/scripts.js
@@ -1,3 +1,15 @@
+/*function toggle(control){
+			var elem = document.getElementById(control);
+		
+			if(elem.style.display == "none"){
+				elem.style.display = "block";
+			}
+			else{ 
+				elem.style.display = "none";
+			}
+		}*/
+
+
 function toggledisplay (id, indicator) {
     if (document.getElementById) {
         var obj = document.getElementById(id);
